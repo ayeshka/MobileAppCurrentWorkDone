da5effe31ae7e8b905adbad5789ef7acffb3a906
Sasiru <ayeshkasasiru1996@gmail.com>
2019-09-27 18:37:00 +0530

    add search bar

--- src/app/salary/find-salary/find-salary.page.ts
+++ src/app/salary/find-salary/find-salary.page.ts
@@ -11,14 +11,17 @@ import { SalaryService } from '../salary.service';
 export class FindSalaryPage implements OnInit, OnDestroy {
   loadingSalary = false;
   loadedSalary: Salary[];
+ 
+  
+  
   private salarySub: Subscription;
   constructor(private slaryService: SalaryService) { }
  ngOnInit() {
   
    this.salarySub =  this.slaryService.salary.subscribe(salary => {
-
+    this.loadingSalary = true;
        this.loadedSalary = salary;
-
+       console.log(this.loadedSalary);
      });
    }
  ngOnDestroy() {
@@ -28,10 +31,35 @@ export class FindSalaryPage implements OnInit, OnDestroy {
    }
 
    ionViewWillEnter() {
-this.loadingSalary = true;
+//this.loadingSalary = true;
 this.slaryService.fetchSalary().subscribe(() => {
-this.loadingSalary = false;
+//this.loadingSalary = false;
 });
    }
+
+   getItems(ev: any) {
+  
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.loadingSalary = true;
+      this. loadedSalary = this. loadedSalary.filter((item) => {
+        return (item.basicSalary.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+    else {
+      this.salarySub =  this.slaryService.salary.subscribe(salary => {
+        this.loadingSalary = true;
+           this.loadedSalary = salary;
+           console.log(this.loadedSalary);
+         });
+    }
+  }
+
+
+
 
 }
